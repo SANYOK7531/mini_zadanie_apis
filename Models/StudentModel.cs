46ee2a6db55e846748ad6afe97cc54ec16c6c272
Oleksandr Petsa <SANYOK7531@users.noreply.github.com>
2025-10-15 20:19:15 +0200

    Initial commit from cvicenie_mvc folder

---
+++ Models/StudentModel.cs
@@ -0,0 +1,14 @@
+﻿namespace cvicenie_mvc.Models
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Gender { get; set; }
+        public string? City { get; set; }
+        public string? Scores { get; set; }
+        public int Age { get; set; }
+
+
+    }
+}
